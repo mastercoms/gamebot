61baf4886a5b9b69737bcee5583aebfca3d0dab4
mastercoms <mastercoms@tuta.io>
2021-02-03 21:21:40 -0500

    more progress on bot_new

--- bot_new.py
+++ bot_new.py
@@ -4,10 +4,16 @@ import asyncio
 import collections
 import datetime
 import inspect
+import logging
+import os
+
+from threading import Lock
 from typing import Optional, Callable, Any
 
 import discord
-import uvloop as uvloop
+import uvloop
+import pytz
+
 from fuzzywuzzy import fuzz
 
 uvloop.install()
@@ -15,17 +21,56 @@ client = discord.Client()
 
 # defaults
 DEFAULT_COUNTDOWN = 60
-DEFAULT_DELTA = datetime.timedelta(seconds=DEFAULT_COUNTDOWN)
+TIMEZONE = pytz.timezone("America/New_York")
+GAME_ROLES = {
+    "dota": "261137719579770882"
+}
+GAMES = list(GAME_ROLES.keys())
+DEFAULT_GAME = GAMES[0]
+DATE_FORMAT = "%I:%M %p %Z"
+GUILD_ID = os.getenv("DANK_GUILD")
+CHANNEL_ID = os.getenv("DANK_CHANNEL")
 
 client.countdown = DEFAULT_COUNTDOWN
-client.delta = DEFAULT_DELTA
+client.datetime = None
 client.queued = None
+client.guild = None
+client.channel = None
+client.current_game = DEFAULT_GAME
+client.dankers = []
+client.lock = Lock()
+
+log = logging.getLogger()
 
 
 class Data:
     @staticmethod
+    def get_datetime() -> datetime.datetime:
+        return client.datetime
+
+    @staticmethod
+    def get_localtime():
+        return Data.get_datetime().astimezone(tz=TIMEZONE)
+
+    @staticmethod
+    def get_formattime():
+        return Data.get_localtime().strftime(DATE_FORMAT)
+
+    @staticmethod
     def get_queued() -> Optional[asyncio.Task]:
         return client.queued
+
+    @staticmethod
+    def get_counter() -> str:
+        return f"**({len(client.dankers)}/5)**"
+
+    @staticmethod
+    def get_guild() -> discord.Guild:
+        return client.guild
+
+    @staticmethod
+    def get_channel() -> discord.TextChannel:
+        return client.channel
 
 
 class Util:
@@ -34,11 +79,25 @@ class Util:
         return content.startswith("d") and fuzz.partial_ratio(content, "dank") > 70
 
     @staticmethod
-    def run_func(func: Callable) -> Optional[Any]:
+    def run_func(func: Callable, **kwargs) -> Optional[Any]:
         if inspect.iscoroutinefunction(func):
-            return await func()
+            return await func(kwargs)
         else:
-            return func()
+            return func(kwargs)
+
+    @staticmethod
+    def represents_int(s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def get_possessive(user: discord.Member) -> str:
+        name = user.display_name
+        possess = "'" if name.endswith("s") else "'s"
+        return f"{name}{possess}"
 
 
 class Registrations:
@@ -68,7 +127,7 @@ class Registrations:
         return decorator
 
     @staticmethod
-    def try_invoke(store, key) -> bool:
+    def try_invoke(store, key, **kwargs) -> bool:
         invoked = store.get(key)
         if not invoked:
             return False
@@ -76,55 +135,96 @@ class Registrations:
             condition = invoked.get("condition")
             if condition:
                 if not Util.run_func(condition):
-                    return
+                    return False
             callback = invoked["callback"]
         else:
             callback = invoked
-        Util.run_func(callback)
+        Util.run_func(callback, kwargs)
+
+    @staticmethod
+    def try_control(key: str, user: discord.User) -> bool:
+        return Registrations.try_invoke(Registrations.controls, key, user=user)
 
 
-
-@Registrations.control
+@Registrations.control("cancel")
 def control_cancel():
     Data.get_queued().cancel("User canceled")
 
 
-@Registrations.control
+@Registrations.control("now")
 def control_now():
     Data.get_queued().cancel("Running now")
     await Data.get_queued().get_coro()()
 
 
-def try_control(control: str) -> bool:
-    control_func = controls.get(control)
-    if control_func:
-        control_func()
-        return True
+@Registrations.control("leave")
+def control_leave(**kwargs):
+    user = kwargs.get("user")
+    client.dankers.remove(user)
+    Data.get_channel().send(f"{user.display_name} left the dank. {Data.get_counter()}")
+
+
+async def add_to_dank(user: discord.Member, channel: discord.TextChannel):
+    # can't add twice
+    if user in client.dankers:
+        return
+
+    # add to dankers
+    client.dankers.append(user)
+
+    name = user.display_name
+
+    if client.countdown > DEFAULT_COUNTDOWN:
+        # if it's not time to Dank Check yet, display the date.
+        msg = f"{name} can dank at {Data.get_formattime()}. {Data.get_counter()}"
     else:
-        return False
+        # it's time to Dank Check!
+        msg = f"{name} is ready to dank. {Data.get_counter()}"
+        # refresh the countdown!
+        client.countdown = DEFAULT_COUNTDOWN
+
+    # send whatever message we need.
+    await channel.send(msg)
 
 
 @client.event
 async def on_message(message):
+    # must not be ourself or a bot
     if message.author == client.user or message.author.bot:
+        return
+
+    # must be in designated channel
+    if message.channel.id != CHANNEL_ID:
         return
 
     # normalize
     message.content = message.content.lower().strip()
 
-    # disallow deviations disregarding d
-    if not is_dank(message.content):
+    if not Util.is_dank(message.content):
         return
 
-    # if we are danking already, allow for the control commands
-    if client.queued:
-        # control word succeeded
-        if try_control(message.content.split(" ")[-1]):
+    # do not let dank data to be concurrently accessed
+    with client.lock:
+        # if we are danking already, allow for the control commands
+        if client.queued:
+            # if the control word succeeded, nothing else to do
+            if Registrations.try_control(message.content.split(" ")[-1], message.author):
+                return
+            # if not, opt into the dank
+            await add_to_dank(message.author, message.channel)
             return
 
+        # start a new dank
 
 
+@client.event
+async def on_ready():
+    client.guild = discord.utils.get(client.guilds, id=GUILD_ID)
+    if not client.guild:
+        log.error("Guild not found")
+        exit(1)
 
-
-
-
+    client.channel = discord.utils.get(Data.get_guild().text_channels, id=CHANNEL_ID)
+    if not client.channel:
+        log.error("Channel not found")
+        exit(1)
